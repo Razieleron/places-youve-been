e0adac7bc89e995eea47b859ccc063f477267a16
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-01-23 16:18:31 -0800

    um...  do some more stuff.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
  // Business Logic 
-function Map() {
+function Atlas() {
   this.places = {}
 } 
 
+Atlas.prototype.addPlace = function(place) {
+  this.places[place.placeName] = place;
+}
+
+//this.places {america.placeName} = america 
+
+// Business Logic for Places
 function Place(placeName, countryName, cityName, languageSpoken, landmarkName, timeOfYear, transportationMethod) {
   this.placeName = placeName;
   this.countryName = countryName;
@@ -10,6 +17,28 @@ function Place(placeName, countryName, cityName, languageSpoken, landmarkName, t
   this.languageSpoken = languageSpoken;
   this.landmarkName = landmarkName;
   this.timeOfYear = timeOfYear;
-  this.transportationMethod = transportationMethod
+  this.transportationMethod = transportationMethod;
 }
 
+Place.prototype.location = function() {
+  return this.cityName + ", " + this.countryName;
+}
+
+Place.prototype.countryNameChange = function(method) {
+  this.countryName = method;
+}
+
+Place.prototype.transportationChange = function(method) {
+  this.transportationMethod = method;
+}
+
+
+
+
+
+Place.prototype.languageToVisit = function(language) {
+  if (this.languageSpoken = language) {
+    return "you can visit " + this.cityName + ", " + this.countryName + " without fear!"
+  }
+  return "don't visit" + this.cityName + ", " + this.countryName + " yet!"
+}
